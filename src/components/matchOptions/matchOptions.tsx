a051cef53d766297501a95d79bc94c89daff2c20
Melissa Coomans <melissa.coomans@capgemini.com>
2023-02-09 15:00:16 +0100

    add data

--- src/components/matchOptions/matchOptions.tsx
+++ src/components/matchOptions/matchOptions.tsx
@@ -1,11 +1,45 @@
 import data from "../../assets/data.json";
 export const MatchOptions = () => {
   const title = "Your Matches:";
+  const final = [];
+  for (let item of data) {
+    final.push(
+      <div key={item.toString()}>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={item.Arnhem[2][12].profile}
+            className="card-img-top"
+            alt=""
+          />
+          <div className="card-body">
+            <h5 className="card-title">{item.Arnhem[2][12].name}</h5>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                Ophaal locatie: {item.Arnhem[2][12].location}
+              </li>
+              <li className="list-group-item">
+                Auto: {item.Arnhem[2][12].car}
+              </li>
+              <li className="list-group-item">
+                Spots total: {item.Arnhem[2][12].spots.total}
+              </li>
+              <li className="list-group-item">
+                Spots left: {item.Arnhem[2][12].spots.taken}
+              </li>
+            </ul>
+            <div className="card-body">
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   // data.map((item) => {
-  //   console.log(item);
+  //   console.log(item.Arnhem[2][14][0].name);
   // });
-  const matchOptions = data;
-  console.log(matchOptions);
   // const users = ["user1", "user2", "user3"];
   // const final = [];
   // for (let user of users) {
@@ -15,17 +49,7 @@ export const MatchOptions = () => {
     <div className="container">
       <h1> {title}</h1>
       <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">Voornaam Achternaam</h5>
-              <p className="card-text">Ophaal locatie: Amsterdam</p>
-              <p className="card-text">Auto: Nissan Juke</p>
-              <p className="card-text">Co2: </p>
-            </div>
-          </div>
-        </div>
+        <div>{final}</div>
       </div>
     </div>
   );
