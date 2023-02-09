f2cf301e8f298fe5ff9def14e141e937b44dd384
Melissa Coomans <melissa.coomans@capgemini.com>
2023-02-09 11:28:01 +0100

    match options component

---
+++ src/components/match-options/matchOptions.tsx
@@ -0,0 +1,15 @@
+export const matchOptions = () => {
+  //JS functies hier
+  const title = "dit is een title";
+
+  return (
+    //HTML hier
+    <div>
+      <h1>{title}</h1>;
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque sunt
+        dolore totam odio suscipit.
+      </p>
+    </div>
+  );
+};
