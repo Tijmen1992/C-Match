7854a55512d48d10762d9d4fb75a50f29290f0f5
Jorine Froeling <jorine.froeling@capgemini.com>
2023-02-09 13:48:29 +0100

    Profiel combo animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { TestComponent } from "./components/testComponent/TestComponent";
 import { Navbar } from "./components/navbar/Navbar";
+import { ProfileAnim } from "./components/profileAnim/ProfileAnim";
 
 function App() {
   return <>
   <Navbar />
   <TestComponent />
+  <ProfileAnim />
   </>; 
 }
 
