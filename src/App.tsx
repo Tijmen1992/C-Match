8c47fa45ae46a93cfc61b8a80612ed15f55a5619
tijmen jacobs <tijmen.jacobs@capgemini.com>
2023-02-09 13:22:18 +0100

    contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { ProfileComponent } from "./components/profileComponent/ProfileComponent
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
-  return <>
-    <Navbar />
-    <ProfileComponent />
-  </>;
+  return (
+    <>
+      <Navbar />
+      <ProfileComponent />
+    </>
+  );
 }
 
 export default App;
