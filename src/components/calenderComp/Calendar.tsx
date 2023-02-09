cbf5516f12de640cd83dfe7965d3f23b85d049f7
Moawad <gabriel.moawad@capgemini.com>
2023-02-09 14:36:01 +0100

    Fixed FINALLY date selector.

--- src/components/calenderComp/Calendar.tsx
+++ src/components/calenderComp/Calendar.tsx
@@ -7,10 +7,20 @@ const Calendar = () => {
 
   let dates = [];
 
-  for (let i = 1; i <= 28; i++) {
-    dates.push({ day: i });
+  let availableDates = data.map(data => data.Arnhem[2]);
+  function gatherTopLevelNumbers(obj: any): string[] {
+    return Object.keys(obj);
   }
 
+  let dateArray = gatherTopLevelNumbers(availableDates[0]);
+  let numberArray = [];
+  for (var i = 0; i < dateArray.length; i++) {
+    numberArray.push(parseInt(dateArray[i]));
+  }
+
+  for (let i = 1; i <= 28; i++) {
+    dates.push({ "day": i, "available": numberArray.includes(i) });
+  }
   return (
     <div className="calendar">
       <h3 className="calendar__month">Februari</h3>
