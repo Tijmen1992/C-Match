466d6d978c9761add9984dbd40568ca02191bcae
Moawad <gabriel.moawad@capgemini.com>
2023-02-09 13:19:53 +0100

    homepage tweak.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { ProfileComponent } from "./components/profileComponent/ProfileComponent";
-import { TestComponent } from "./components/testComponent/TestComponent";
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return <>
     <Navbar />
-    <TestComponent />
     <ProfileComponent />
   </>;
 }
